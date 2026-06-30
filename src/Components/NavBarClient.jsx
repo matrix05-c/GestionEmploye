50f9615beab59faf6c8869c326b0c04176eded7c
matrix05-c <caddyrasolonjatovo@gmail.com>
2026-06-30 22:25:28 +0300

    Historix fixed

--- src/Components/NavBarClient.jsx
+++ src/Components/NavBarClient.jsx
@@ -3,27 +3,27 @@ import Logo from "../assets/notifications.svg"
 function NavBarClient() {
     return (
         <>
-            <h6 className="text-white mt-3 mx-2 mx-lg-4">Espace</h6>
-            <div className="d-flex mx-2 mx-lg-4 mt-1 pb-2 justify-content-between" style={{ borderBottom: "red solid 2px" }}>
-                <h2 className="text-white fw-bold">MON COMPTE</h2>
-                <img src={Logo} alt="" />
-            </div>
+                <h6 className="text-white mt-3 mx-2 mx-lg-4">Espace</h6>
+                <div className="d-flex mx-2 mx-lg-4 mt-1 pb-2 justify-content-between" style={{ borderBottom: "red solid 2px" }}>
+                    <h2 className="text-white fw-bold">MON COMPTE</h2>
+                    <img src={Logo} alt="" />
+                </div>
 
-            <div className="d-flex mt-3 mx-2 mx-lg-4 gap-lg-3 gap-1">
-                <button className="btn px-1 px-md-3 same-btnPolice same-btn btn-dark rounded-0" style={{ border: "red solid 2px" }}>
-                    <NavLink to={"AppercuClient"} className={"nav-link"}>
-                        Apercu
-                    </NavLink>
-                </button>
+                <div className="d-flex mt-3 mx-2 mx-lg-4 gap-lg-3 gap-1">
+                    <button className="btn px-1 px-md-3 same-btnPolice same-btn btn-dark rounded-0" style={{ border: "red solid 2px" }}>
+                        <NavLink to={"AppercuClient"} className={"nav-link"}>
+                            Apercu
+                        </NavLink>
+                    </button>
 
-                <button className="btn px-1 px-md-3 same-btnPolice same-btn btn-dark rounded-0" style={{ border: "red solid 2px" }}>
-                    Historique
-                </button>
+                    <button className="btn px-1 px-md-3 same-btnPolice same-btn btn-dark rounded-0" style={{ border: "red solid 2px" }}>
+                        Historique
+                    </button>
 
-                <button className="btn px-1 px-md-3 same-btnPolice same-btn btn-dark rounded-0" style={{ border: "red solid 2px" }}>
-                    Profil
-                </button>
-            </div>
+                    <button className="btn px-1 px-md-3 same-btnPolice same-btn btn-dark rounded-0" style={{ border: "red solid 2px" }}>
+                        Profil
+                    </button>
+                </div>
 
             <Outlet />
         </>
