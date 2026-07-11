954a999a320ac2e26a5b56f3988631e2af930161
matrix05-c <caddyrasolonjatovo@gmail.com>
2026-07-12 00:10:33 +0300

    login

--- src/View/Accueil.jsx
+++ src/View/Accueil.jsx
@@ -38,34 +38,39 @@ function Accueil() {
 
     return (
         <>
-            {/* <h1 className="text-center mb-5 mt-5 text-white">Login</h1> */}
-            <NavBarClient login={false}></NavBarClient>
+            <div style={{ overflow: "hidden" }}>
 
-            <div className="card mt-5 p-4 w-50 mx-auto d-flex flex-row align-items-center justify-content-center">
-                <div className="w-50 pe-1 mt-5">
+                {/* <h1 className="text-center mb-5 mt-5 text-white">Login</h1> */}
+                <NavBarClient login={false}></NavBarClient>
 
-                    <div className="d-flex align-items-center mb-3">
-                        <label className="me-3" style={{ width: "100px" }}>Email: </label>
-                        <div className="form-outline" >
-                            <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Entrer votre email" className="form-control" />
+                <div className="d-flex align-items-center justify-content-center" style={{ minHeight: "70vh" }}>
+                    <div className="card p-4 w-50 mx-auto d-flex flex-md-row flex-column flex-column-reverse">
+                        <div className="w-50 pe-1 mt-5">
+
+                            <div className="d-flex align-items-center mb-3">
+                                <label className="me-3" style={{ width: "100px" }}>Email: </label>
+                                <div className="form-outline" >
+                                    <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="Entrer votre email" className="form-control" />
+                                </div>
+                            </div>
+
+                            <div className="d-flex align-items-center mb-3">
+                                <label className="me-3" style={{ width: "100px" }}>Password: </label>
+                                <div className="form-outline" >
+                                    <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="password" className="form-control" />
+                                </div>
+                            </div>
+
+                            <div className="mt-4 mb-5" style={{ marginLeft: "" }}>
+                                <button onClick={handleLogin} className="btn btn-primary w-100">Login</button>
+
+                            </div>
+                        </div>
+                        <div className="w-50 p-0 d-flex">
+                            <img src={photo} id="loginPhoto" alt="loginPhoto" className="w-100 h-100"
+                                style={{}} />
                         </div>
                     </div>
-
-                    <div className="d-flex align-items-center mb-3">
-                        <label className="me-3" style={{ width: "100px" }}>Password: </label>
-                        <div className="form-outline" >
-                            <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder="password" className="form-control" />
-                        </div>
-                    </div>
-
-                    <div className="mt-4 mb-5" style={{ marginLeft: "" }}>
-                        <button onClick={handleLogin} className="btn btn-primary w-100">Login</button>
-
-                    </div>
-                </div>
-                <div className="w-50 p-0 d-flex">
-                    <img src={photo} alt="loginPhoto" className="w-100 h-100"
-                        style={{}} />
                 </div>
             </div>
         </>
